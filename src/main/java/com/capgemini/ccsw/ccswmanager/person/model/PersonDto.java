5f3730b883cfd8a6adbe5d8efe3964e9404977c5
CORP\asolerpa <andrea.soler-paz-external@capgemini.com>
2022-01-13 12:32:05 +0100

    Creación pantalla básica equipo

--- src/main/java/com/capgemini/ccsw/ccswmanager/person/model/PersonDto.java
+++ src/main/java/com/capgemini/ccsw/ccswmanager/person/model/PersonDto.java
@@ -1,186 +1,280 @@
 package com.capgemini.ccsw.ccswmanager.person.model;
 
+import com.capgemini.ccsw.ccswmanager.center.model.CenterDto;
+
 /**
  * @author aolmosca
  *
  */
 public class PersonDto {
-   private Integer id;
+  private Integer id;
 
-   private String saga;
+  private String saga;
 
-   private String username;
+  private String username;
 
-   private String email;
+  private String email;
 
-   private String name;
+  private String name;
 
-   private String lastname;
+  private String lastname;
 
-   private Integer centerId;
+  private CenterDto center;
 
-   private String businesscode;
+  private String businesscode;
 
-   private boolean active;
+  private Integer active;
 
-   private String grade;
+  private String grade;
 
-   /**
+  private String customer;
+
+  private String role;
+
+  private String details;
+
+  private Integer hours;
+
+  private String department;
+
+  /**
    * @return id
    */
-   public Integer getId() {
+  public Integer getId() {
 
-      return this.id;
-   }
+    return this.id;
+  }
 
-   /**
+  /**
    * @param id new value of {@link #getid}.
    */
-   public void setId(Integer id) {
+  public void setId(Integer id) {
 
-      this.id = id;
-   }
+    this.id = id;
+  }
 
-   /**
+  /**
    * @return saga
    */
-   public String getSaga() {
+  public String getSaga() {
 
-      return this.saga;
-   }
+    return this.saga;
+  }
 
-   /**
+  /**
    * @param saga new value of {@link #getsaga}.
    */
-   public void setSaga(String saga) {
+  public void setSaga(String saga) {
 
-      this.saga = saga;
-   }
+    this.saga = saga;
+  }
 
-   /**
+  /**
    * @return username
    */
-   public String getUsername() {
+  public String getUsername() {
 
-      return this.username;
-   }
+    return this.username;
+  }
 
-   /**
+  /**
    * @param username new value of {@link #getusername}.
    */
-   public void setUsername(String username) {
+  public void setUsername(String username) {
 
-      this.username = username;
-   }
+    this.username = username;
+  }
 
-   /**
+  /**
    * @return email
    */
-   public String getEmail() {
+  public String getEmail() {
 
-      return this.email;
-   }
+    return this.email;
+  }
 
-   /**
+  /**
    * @param email new value of {@link #getemail}.
    */
-   public void setEmail(String email) {
+  public void setEmail(String email) {
 
-      this.email = email;
-   }
+    this.email = email;
+  }
 
-   /**
+  /**
    * @return name
    */
-   public String getName() {
+  public String getName() {
 
-      return this.name;
-   }
+    return this.name;
+  }
 
-   /**
+  /**
    * @param name new value of {@link #getname}.
    */
-   public void setName(String name) {
+  public void setName(String name) {
 
-      this.name = name;
-   }
+    this.name = name;
+  }
 
-   /**
+  /**
    * @return lastname
    */
-   public String getLastname() {
+  public String getLastname() {
 
-      return this.lastname;
-   }
+    return this.lastname;
+  }
 
-   /**
+  /**
    * @param lastname new value of {@link #getlastname}.
    */
-   public void setLastname(String lastname) {
+  public void setLastname(String lastname) {
 
-      this.lastname = lastname;
-   }
+    this.lastname = lastname;
+  }
 
-   /**
+  /**
    * @return center_id
    */
-   public Integer getCenterId() {
+  public CenterDto getCenter() {
 
-      return this.centerId;
-   }
+    return this.center;
+  }
 
-   /**
+  /**
    * @param center_id new value of {@link #getcenter_id}.
    */
-   public void setCenterId(Integer centerId) {
+  public void setCenter(CenterDto center) {
 
-      this.centerId = centerId;
-   }
+    this.center = center;
+  }
 
-   /**
+  /**
    * @return businesscode
    */
-   public String getBusinesscode() {
+  public String getBusinesscode() {
 
-      return this.businesscode;
-   }
+    return this.businesscode;
+  }
 
-   /**
+  /**
    * @param businesscode new value of {@link #getbusinesscode}.
    */
-   public void setBusinesscode(String businesscode) {
+  public void setBusinesscode(String businesscode) {
 
-      this.businesscode = businesscode;
-   }
+    this.businesscode = businesscode;
+  }
 
-   /**
+  /**
    * @return active
    */
-   public boolean getActive() {
+  public Integer getActive() {
 
-      return this.active;
-   }
+    return this.active;
+  }
 
-   /**
+  /**
    * @param active new value of {@link #getactive}.
    */
-   public void setActive(boolean active) {
+  public void setActive(Integer active) {
 
-      this.active = active;
-   }
+    this.active = active;
+  }
 
-   /**
-    * @return the grade
-    */
-   public String getGrade() {
-      return grade;
-   }
+  /**
+   * @return the grade
+   */
+  public String getGrade() {
 
-   /**
-    * @param grade the grade to set
-    */
-   public void setGrade(String grade) {
-      this.grade = grade;
-   }
+    return this.grade;
+  }
+
+  /**
+   * @param grade the grade to set
+   */
+  public void setGrade(String grade) {
+
+    this.grade = grade;
+  }
+
+  /**
+   * @return customer
+   */
+  public String getCustomer() {
+
+    return this.customer;
+  }
+
+  /**
+   * @param customer new value of {@link #getcustomer}.
+   */
+  public void setCustomer(String customer) {
+
+    this.customer = customer;
+  }
+
+  /**
+   * @return role
+   */
+  public String getRole() {
+
+    return this.role;
+  }
+
+  /**
+   * @param role new value of {@link #getrole}.
+   */
+  public void setRole(String role) {
+
+    this.role = role;
+  }
+
+  /**
+   * @return details
+   */
+  public String getDetails() {
+
+    return this.details;
+  }
+
+  /**
+   * @param details new value of {@link #getdetails}.
+   */
+  public void setDetails(String details) {
+
+    this.details = details;
+  }
+
+  /**
+   * @return hours
+   */
+  public Integer getHours() {
+
+    return this.hours;
+  }
+
+  /**
+   * @param hours new value of {@link #gethours}.
+   */
+  public void setHours(Integer hours) {
+
+    this.hours = hours;
+  }
+
+  /**
+   * @return department
+   */
+  public String getDepartment() {
+
+    return this.department;
+  }
+
+  /**
+   * @param department new value of {@link #getdepartment}.
+   */
+  public void setDepartment(String department) {
+
+    this.department = department;
+  }
 
 }
