dcfcd2c5b68d54f4c8a7f54d763b89388748e1d5
CORP\asolerpa <andrea.soler-paz-external@capgemini.com>
2022-01-25 09:42:11 +0100

    Edición pantalla equipo

--- src/main/java/com/capgemini/ccsw/ccswmanager/person/PersonController.java
+++ src/main/java/com/capgemini/ccsw/ccswmanager/person/PersonController.java
@@ -3,11 +3,14 @@ package com.capgemini.ccsw.ccswmanager.person;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capgemini.ccsw.ccswmanager.config.mapper.BeanMapper;
 import com.capgemini.ccsw.ccswmanager.person.model.PersonDto;
+import com.capgemini.ccsw.ccswmanager.person.model.PersonEntity;
 
 /**
  * TODO asolerpa This type ...
@@ -19,6 +22,9 @@ import com.capgemini.ccsw.ccswmanager.person.model.PersonDto;
 public class PersonController {
 
   @Autowired
+  private BeanMapper beanMapper;
+
+  @Autowired
   private PersonService personService;
 
   @RequestMapping(path = "/", method = RequestMethod.GET)
@@ -26,6 +32,17 @@ public class PersonController {
 
     return this.personService.findPersons();
 
+  }
+
+  /**
+   * @param person
+   * @return
+   */
+  @RequestMapping(path = "/", method = RequestMethod.POST)
+  public PersonDto saveOrUpdatePerson(@RequestBody PersonDto person) {
+
+    PersonEntity entity = this.personService.saveOrUpdatePerson(person);
+    return this.beanMapper.map(entity, PersonDto.class);
   }
 
 }
--- src/main/java/com/capgemini/ccsw/ccswmanager/person/PersonServiceImpl.java
+++ src/main/java/com/capgemini/ccsw/ccswmanager/person/PersonServiceImpl.java
@@ -1,12 +1,15 @@
 package com.capgemini.ccsw.ccswmanager.person;
 
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.capgemini.ccsw.ccswmanager.config.mapper.BeanMapper;
 import com.capgemini.ccsw.ccswmanager.person.model.PersonDto;
+import com.capgemini.ccsw.ccswmanager.person.model.PersonEntity;
 
 /**
  * @author aolmosca
@@ -25,6 +28,30 @@ public class PersonServiceImpl implements PersonService {
   public List<PersonDto> findPersons() {
 
     return this.beanMapper.mapList(this.personRepository.findAll(), PersonDto.class);
+  }
+
+  @Override
+  public PersonEntity get(long id) {
+
+    return this.personRepository.findById(id).orElse(null);
+  }
+
+  @Override
+  public PersonEntity saveOrUpdatePerson(PersonDto personTo) {
+
+    Objects.requireNonNull(personTo, "person");
+
+    PersonEntity person = null;
+    if (personTo.getId() != null)
+      person = get(personTo.getId());
+
+    if (person == null) {
+      person = new PersonEntity();
+    }
+
+    BeanUtils.copyProperties(personTo, person, "id", "center");
+
+    return this.personRepository.save(person);
   }
 
 }
