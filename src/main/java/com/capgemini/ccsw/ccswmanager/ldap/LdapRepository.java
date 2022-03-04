c4ad9a4c50e1a7b416c6adf68508c97f64bbd5dd
danipalm <99872742+danipalm@users.noreply.github.com>
2022-03-04 09:34:59 +0100

    Query sentencias con OR

--- src/main/java/com/capgemini/ccsw/ccswmanager/ldap/LdapRepository.java
+++ src/main/java/com/capgemini/ccsw/ccswmanager/ldap/LdapRepository.java
@@ -18,7 +18,7 @@ public interface LdapRepository extends CrudRepository<PersonEntity, Long> {
 	@Query(value = "SELECT t1.username, t1.name, t1.lastname " //
 	         + "FROM person t1 " + "WHERE NOT EXISTS (SELECT NULL " //
 	         + "FROM t_members t2 " //
-	         + "WHERE t2.user_cn = t1.username and t2.group_cn = 'dlesccsw') and (t1.grade!='') and t1.department = 'CCSw' and active = 1 " //
+	         + "WHERE t2.user_cn = t1.username and t2.group_cn = 'dlesccsw') and (t1.grade!='' OR t1.grade IS not null) and t1.department = 'CCSw' and active = 1 " //
 	         + "ORDER BY t1.lastname, t1.name", nativeQuery = true)
   List<LdapPerson> comparePersonsToLdap();
 
@@ -26,14 +26,14 @@ public interface LdapRepository extends CrudRepository<PersonEntity, Long> {
 	         + "from t_members m " //
 	         + "join t_person t on m.user_cn = t.username " //
 	         + "where m.group_cn = 'dlesccsw' " //
-	         + "and not exists (select 1 from person p where p.username = m.user_cn and (p.grade!='' or p.grade is not null)  and p.department = 'CCSw' and p.active = 1) " //
+	         + "and not exists (select 1 from person p where p.username = m.user_cn and (p.grade!='' OR p.grade is not null)  and p.department = 'CCSw' and p.active = 1) " //
 	         + "ORDER BY t.lastname, t.name", nativeQuery = true)         
   List<LdapPerson> compareLdapToPersons();
 	 
 	 @Query(value = "SELECT t1.username, t1.name, t1.lastname " //
 	         + "FROM person t1 " + "WHERE NOT EXISTS (SELECT NULL " //
 	         + "FROM t_members t2 " //
-	         + "WHERE t2.user_cn = t1.username and t2.group_cn = 'dlesccsw.becarios') and (t1.grade='')  and t1.department = 'CCSw' and active = 1 " //
+	         + "WHERE t2.user_cn = t1.username and t2.group_cn = 'dlesccsw.becarios') and (t1.grade='' OR t1.grade IS null )  and t1.department = 'CCSw' and active = 1 " //
 	         + "ORDER BY t1.lastname, t1.name", nativeQuery = true)
   List<LdapPerson> comparePersonsToLdapBecarios();
 
@@ -41,7 +41,7 @@ public interface LdapRepository extends CrudRepository<PersonEntity, Long> {
 	         + "from t_members m " //
 	         + "join t_person t on m.user_cn = t.username " //
 	         + "where m.group_cn = 'dlesccsw.becarios' " //
-	         + "and not exists (select 1 from person p where p.username = m.user_cn and (p.grade='') and p.department = 'CCSw' and p.active = 1) " //
+	         + "and not exists (select 1 from person p where p.username = m.user_cn and (p.grade='' OR p.grade IS null) and p.department = 'CCSw' and p.active = 1) " //
 	         + "ORDER BY t.lastname, t.name", nativeQuery = true)         
   List<LdapPerson> compareLdapToPersonsBecarios();
 
