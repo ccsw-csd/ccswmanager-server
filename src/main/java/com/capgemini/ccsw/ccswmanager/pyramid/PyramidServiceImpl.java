8b9245a572b237be32620ec3801afad244233554
CORP\jchengli <juan-juanxin.cheng-liu-external@capgemini.com>
2022-03-17 11:38:58 +0100

    creacion pantalla pyramid

---
+++ src/main/java/com/capgemini/ccsw/ccswmanager/pyramid/PyramidServiceImpl.java
@@ -0,0 +1,5 @@
+package com.capgemini.ccsw.ccswmanager.pyramid;
+
+public class PyramidServiceImpl {
+
+}
---
+++ src/main/java/com/capgemini/ccsw/ccswmanager/pyramid/model/PyramidCostDto.java
@@ -0,0 +1,5 @@
+package com.capgemini.ccsw.ccswmanager.pyramid.model;
+
+public class PyramidCostDto {
+
+}
