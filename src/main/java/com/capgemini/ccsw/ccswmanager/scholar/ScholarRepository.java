945456698d6dba1a41c39fe0ae23c62ac6a0638d
danipalm <99872742+danipalm@users.noreply.github.com>
2022-03-18 10:49:26 +0100

    Fallo merge resuelto

--- src/main/java/com/capgemini/ccsw/ccswmanager/scholar/ScholarRepository.java
+++ src/main/java/com/capgemini/ccsw/ccswmanager/scholar/ScholarRepository.java
@@ -2,7 +2,6 @@ package com.capgemini.ccsw.ccswmanager.scholar;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import com.capgemini.ccsw.ccswmanager.scholar.model.ScholarEntity;
@@ -12,6 +11,5 @@ public interface ScholarRepository extends CrudRepository<ScholarEntity, Long> {
     @Override
     List<ScholarEntity> findAll();
 
-    @EntityGraph(attributePaths = { "person" })
-    ScholarEntity getByPerson_Id(Long id);
+    ScholarEntity getByPersonId(Long id);
 }
