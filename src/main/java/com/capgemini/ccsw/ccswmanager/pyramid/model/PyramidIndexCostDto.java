d32f3680451d875a6c38725cfd48cb8f9b7f7369
CORP\jchengli <juan-juanxin.cheng-liu-external@capgemini.com>
2022-03-23 13:43:56 +0100

    pantalla pyramid faltaba 1 archivo

---
+++ src/main/java/com/capgemini/ccsw/ccswmanager/pyramid/model/PyramidIndexCostDto.java
@@ -0,0 +1,118 @@
+package com.capgemini.ccsw.ccswmanager.pyramid.model;
+
+/**
+ * @author jchengli
+ *
+ */
+public class PyramidIndexCostDto {
+    private String rowName;
+
+    private Double valueA1;
+
+    private Double valueA2;
+
+    private Double valueB1;
+
+    private Double valueB2;
+
+    private Double valueB3;
+
+    private Double valueC1;
+
+    private Double valueC2;
+
+    private Double valueC3;
+
+    private Double valueD1;
+
+    private Double valueD2;
+
+    public String getRowName() {
+        return rowName;
+    }
+
+    public void setRowName(String rowName) {
+        this.rowName = rowName;
+    }
+
+    public Double getValueA1() {
+        return valueA1;
+    }
+
+    public void setValueA1(Double valueA1) {
+        this.valueA1 = valueA1;
+    }
+
+    public Double getValueA2() {
+        return valueA2;
+    }
+
+    public void setValueA2(Double valueA2) {
+        this.valueA2 = valueA2;
+    }
+
+    public Double getValueB1() {
+        return valueB1;
+    }
+
+    public void setValueB1(Double valueB1) {
+        this.valueB1 = valueB1;
+    }
+
+    public Double getValueB2() {
+        return valueB2;
+    }
+
+    public void setValueB2(Double valueB2) {
+        this.valueB2 = valueB2;
+    }
+
+    public Double getValueB3() {
+        return valueB3;
+    }
+
+    public void setValueB3(Double valueB3) {
+        this.valueB3 = valueB3;
+    }
+
+    public Double getValueC1() {
+        return valueC1;
+    }
+
+    public void setValueC1(Double valueC1) {
+        this.valueC1 = valueC1;
+    }
+
+    public Double getValueC2() {
+        return valueC2;
+    }
+
+    public void setValueC2(Double valueC2) {
+        this.valueC2 = valueC2;
+    }
+
+    public Double getValueC3() {
+        return valueC3;
+    }
+
+    public void setValueC3(Double valueC3) {
+        this.valueC3 = valueC3;
+    }
+
+    public Double getValueD1() {
+        return valueD1;
+    }
+
+    public void setValueD1(Double valueD1) {
+        this.valueD1 = valueD1;
+    }
+
+    public Double getValueD2() {
+        return valueD2;
+    }
+
+    public void setValueD2(Double valueD2) {
+        this.valueD2 = valueD2;
+    }
+
+}
