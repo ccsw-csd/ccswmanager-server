0c9dbc3f8c880914d9cdfd42b8d33195249cd51c
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2022-03-31 11:53:56 +0200

    Finished

--- src/main/java/com/capgemini/ccsw/ccswmanager/center/CenterServiceImpl.java
+++ src/main/java/com/capgemini/ccsw/ccswmanager/center/CenterServiceImpl.java
@@ -2,8 +2,6 @@ package com.capgemini.ccsw.ccswmanager.center;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +16,6 @@ public class CenterServiceImpl implements CenterService {
 
     @Autowired
     CenterRepository centerRepository;
-
-    private static final Logger LOG = LoggerFactory.getLogger(CenterServiceImpl.class);
 
     @Override
     public List<CenterEntity> findAll() {
