443dfe4ff66f3304f54cf67b58f7bec40be64a91
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2023-02-09 15:04:27 +0100

    Fix path

--- src/main/java/com/ccsw/ccswmanager/technology/TechnologyController.java
+++ src/main/java/com/ccsw/ccswmanager/technology/TechnologyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping(value = "/Technology")
+@RequestMapping(value = "/technology")
 @RestController
 public class TechnologyController {
 
