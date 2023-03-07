ae900129bd024ed69de280d67348e0d0b78bb571
Nikolai <the-inic@hotmail.com>
2023-03-07 12:58:17 +0100

    Actualizado error de mensajes

--- src/main/java/com/ccsw/ccswmanager/educationcenter/EducationCenterServiceImpl.java
+++ src/main/java/com/ccsw/ccswmanager/educationcenter/EducationCenterServiceImpl.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ccsw.ccswmanager.common.exception.AlreadyExistsException;
+import com.ccsw.ccswmanager.common.exception.ConflictOnDeletionException;
 import com.ccsw.ccswmanager.educationcenter.model.EducationCenterEntity;
 import com.ccsw.ccswmanager.intern.InternService;
-import com.ccsw.ccswmanager.utils.ItemInUseException;
 
 @Service
 public class EducationCenterServiceImpl implements EducationCenterService {
@@ -31,21 +32,22 @@ public class EducationCenterServiceImpl implements EducationCenterService {
     }
 
     @Override
-    public EducationCenterEntity save(EducationCenterEntity entity) throws ItemInUseException {
+    public EducationCenterEntity save(EducationCenterEntity entity) throws AlreadyExistsException {
 
         EducationCenterEntity educationCenter = this.repository.findByName(entity.getName());
 
         if (educationCenter != null && (entity.getId() == null || !educationCenter.getId().equals(entity.getId()))) {
-            throw new ItemInUseException();
+            throw new AlreadyExistsException("El nombre ya existe en la BBDD");
         }
         return repository.save(entity);
     }
 
     @Override
-    public void deleteById(Long id) throws ItemInUseException {
+    public void deleteById(Long id) throws ConflictOnDeletionException {
 
         if (this.internService.existsByEducationCenterId(id)) {
-            throw new ItemInUseException();
+            throw new ConflictOnDeletionException(
+                    "No se puede borrar la titulación porque está relacionada con un becario");
         }
         repository.deleteById(id);
     }
