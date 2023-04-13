3b36bc291317996f16d0ad1e0a03f3e6ffe2310e
Nikolai <the-inic@hotmail.com>
2023-04-13 15:38:21 +0200

    TrancodeController

--- src/main/java/com/ccsw/ccswmanager/centertranscode/CenterTranscodeService.java
+++ src/main/java/com/ccsw/ccswmanager/centertranscode/CenterTranscodeService.java
@@ -1,5 +1,8 @@
 package com.ccsw.ccswmanager.centertranscode;
 
+import com.ccsw.ccswmanager.center.model.CenterEntity;
+
 public interface CenterTranscodeService {
 
+    CenterEntity findByName(String name);
 }
