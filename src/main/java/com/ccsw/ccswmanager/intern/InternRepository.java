d79f21825f11ed66850c561d0946de480c18683b
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2023-04-20 08:49:24 +0200

    Fix query

--- src/main/java/com/ccsw/ccswmanager/intern/InternRepository.java
+++ src/main/java/com/ccsw/ccswmanager/intern/InternRepository.java
@@ -1,22 +1,19 @@
 package com.ccsw.ccswmanager.intern;
 
-import java.util.List;
-
+import com.ccsw.ccswmanager.intern.model.InternEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-import com.ccsw.ccswmanager.intern.model.InternEntity;
+import java.util.List;
 
 public interface InternRepository extends CrudRepository<InternEntity, Long>, JpaSpecificationExecutor<InternEntity> {
 
     @Override
-    @EntityGraph(attributePaths = { "education", "educationCenter", "center", "province", "technologies",
-            "englishLevel", "action" })
+    @EntityGraph(attributePaths = { "education", "educationCenter", "center", "province", "technologies", "englishLevel", "action" })
     List<InternEntity> findAll();
 
-    @EntityGraph(attributePaths = { "education", "educationCenter", "center", "province", "technologies",
-            "englishLevel", "action" })
+    @EntityGraph(attributePaths = { "education", "educationCenter", "center", "province", "technologies", "englishLevel", "action" })
     List<InternEntity> findByUsernameIsNotNullAndUsernameIsNotAndActive(String username, Integer active);
 
     boolean existsByEducationId(Long educationId);
